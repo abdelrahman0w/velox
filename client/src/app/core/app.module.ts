67f517a3a35a24b7bba19dbe51fed8372e86cb31
MariaGamal <maria.gamal@ejust.edu.eg>
2022-08-29 19:53:59 +0200

    _

--- client/src/app/core/app.module.ts
+++ client/src/app/core/app.module.ts
@@ -1,18 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { AlbumComponent } from '../components/album/album.component';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, AlbumComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
