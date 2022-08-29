3d429aea1dde3fcad0bbbdb87ec7f919c78c988b
Abdelrahman <20213927+Abdelrahman0W@users.noreply.github.com>
2022-08-29 14:28:10 +0200

    structured some files

--- client/src/app/core/app.module.ts
+++ client/src/app/core/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AlbumComponent } from '../album/album.component';
+import { AlbumComponent } from '../components/album/album.component';
 @NgModule({
   declarations: [AppComponent, AlbumComponent],
   imports: [BrowserModule, AppRoutingModule],
