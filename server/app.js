bc44e746cf94563170bef95e264812f4eb90c30f
MariaGamal <maria.gamal@ejust.edu.eg>
2022-08-29 18:58:52 +0200

    app modified

--- server/app.js
+++ server/app.js
@@ -27,11 +27,3 @@ var indexRouter = require('./routes/index');
 app.use(endpoint, indexRouter);
 
 module.exports = app;
-
-
-var server = app.listen(5030, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
-});
