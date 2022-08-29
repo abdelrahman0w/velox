f4064bd94fab62de0f193f79cfe25d60e6d8a8ad
MariaGamal <maria.gamal@ejust.edu.eg>
2022-08-29 13:41:52 +0200

    created-product-schema

--- server/models/product.js
+++ server/models/product.js
@@ -1,6 +1,16 @@
 const mongoose = require('mongoose');
 
 const schema = mongoose.Schema({
+    id: Number,
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    price: { type: Number, required: true },
+    image: { type: String, required: true },
+    category: { type: String, required: true },
+    rating: {
+        type: Map,
+        of: Number,
+    }
 });
 
 module.exports = mongoose.model('product', schema);
