58796a9a66cd6131419db2373401f90835f1cc3b
AhmedBedeir <bederahmed0@gmail.com>
2022-08-30 11:58:28 +0200

    footer created

--- client/src/app/core/app.module.ts
+++ client/src/app/core/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AlbumComponent } from '../components/album/album.component';
+import { FooterComponent } from '../components/footer/footer.component';
 @NgModule({
-  declarations: [AppComponent, AlbumComponent],
+  declarations: [AppComponent, AlbumComponent, FooterComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
