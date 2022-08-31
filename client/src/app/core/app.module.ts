fa07433a360c467ba2afe0735ddd29fcd92befa0
Abdelrahman <20213927+Abdelrahman0W@users.noreply.github.com>
2022-08-31 18:44:06 +0200

    resolve conflict

--- client/src/app/core/app.module.ts
+++ client/src/app/core/app.module.ts
@@ -4,8 +4,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AlbumComponent } from '../components/album/album.component';
 import { CartComponent } from '../components/cart/cart.component';
+import { FooterComponent } from '../components/footer/footer.component';
+
 @NgModule({
-  declarations: [AppComponent, AlbumComponent, CartComponent],
+  declarations: [AppComponent, AlbumComponent, CartComponent, FooterComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
