78898bb6ee939dd617ef85faad1057d48091c458
Abdelrahman <20213927+Abdelrahman0W@users.noreply.github.com>
2022-09-01 22:34:08 +0200

    header created

--- client/src/app/components/header/header.component.ts
+++ client/src/app/components/header/header.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnChanges {
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  @Input() items: number = 0;
+
+  ngOnChanges(): void {
   }
 
 }
