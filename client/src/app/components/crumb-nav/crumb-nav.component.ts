11795005c7b9044f5d90802f302178da6037139b
Abdelrahman <20213927+Abdelrahman0W@users.noreply.github.com>
2022-09-03 06:39:35 +0200

    _

--- client/src/app/components/crumb-nav/crumb-nav.component.ts
+++ client/src/app/components/crumb-nav/crumb-nav.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-crumb-nav',
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./crumb-nav.component.css']
 })
 export class CrumbNavComponent implements OnInit {
+
+  // @Input() crumbs: string[] = [];
+
+  @Input() crnt: string = '';
 
   constructor() { }
 
