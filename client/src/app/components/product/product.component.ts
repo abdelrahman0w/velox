d7921cfb6a463ab477356d40e3fd2083fcf1a2e9
MariaGamal <maria.gamal@ejust.edu.eg>
2022-09-03 05:15:35 +0200

    done

--- client/src/app/components/product/product.component.ts
+++ client/src/app/components/product/product.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ProductsService } from 'src/app/core/products.service';
 
 @Component({
   selector: 'app-product',
@@ -7,27 +9,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
 
-  constructor() { }
+  public href: string = "";
+  public prodID: string = "";
+  public prod: any = {};
+
+  constructor(private router: Router, private prodsService: ProductsService) { }
 
   ngOnInit(): void {
+    this.href = this.router.url;
+    this.prodID = this.href.substring(9);
+    this.prod = this.prodsService.getProduct(parseInt(this.prodID));
   }
 
-  prod = {
-    id: 1,
-    title: 'Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops',
-    price: 109.95,
-    discount: 85,
-    description:
-      'Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday',
-    category: "men's clothing",
-    image: 'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg',
-    qty: 15,
-    new: true,
-    rating: {
-      rate: 3.9,
-      count: 120,
-    }
-  }
+  
+  // console.log(prod.title);
+  
+  // prod = {
+  //   id: 1,
+  //   title: 'Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops',
+  //   price: 109.95,
+  //   discount: 85,
+  //   description:
+  //     'Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday',
+  //   category: "men's clothing",
+  //   image: 'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg',
+  //   qty: 15,
+  //   new: true,
+  //   rating: {
+  //     rate: 3.9,
+  //     count: 120,
+  //   }
+  // }
 
   cart = 0;
   wishList: number[] = [];
