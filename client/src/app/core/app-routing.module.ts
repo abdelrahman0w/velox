71fe35db6a473f36edbe380a9be5107e02255933
Abdelrahman <20213927+Abdelrahman0W@users.noreply.github.com>
2022-09-03 06:13:26 +0200

    res conf

--- client/src/app/core/app-routing.module.ts
+++ client/src/app/core/app-routing.module.ts
@@ -15,7 +15,7 @@ const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'product/:id', component: ProductComponent },
-]
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
