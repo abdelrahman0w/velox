445e646ad3531d26880615a8cfe34eb5d3775320
Abdelrahman <20213927+Abdelrahman0W@users.noreply.github.com>
2022-09-03 03:40:35 +0200

    single product

--- client/src/app/components/product/product.component.ts
+++ client/src/app/components/product/product.component.ts
@@ -12,4 +12,39 @@ export class ProductComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  prod = {
+    id: 1,
+    title: 'Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops',
+    price: 109.95,
+    discount: 85,
+    description:
+      'Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday',
+    category: "men's clothing",
+    image: 'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg',
+    qty: 15,
+    new: true,
+    rating: {
+      rate: 3.9,
+      count: 120,
+    }
+  }
+
+  cart = 0;
+  wishList: number[] = [];
+  wish = this.wishList.length;
+  isInWishList(id: number) {
+    return this.wishList.includes(id);
+  }
+  decQuantity(id: number) {
+    this.prod.qty--;
+    this.cart++;
+  }
+  addWish(id: number) {
+    this.wishList.push(id);
+    this.wish++;
+  }
+  removeWish(id: number) {
+    this.wishList = this.wishList.filter(prod => prod != id);
+    this.wish--;
+  }
 }
